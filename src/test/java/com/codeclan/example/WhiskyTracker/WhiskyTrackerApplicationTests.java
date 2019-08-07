3f3966d4fb235ea43286cacc3c3fda29b544d2b5
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-07 16:12:45 +0100

    Added query to get all whiskies for a particular year.

--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,16 +1,38 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WhiskyTrackerApplicationTests {
 
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void canFindWhiskiesByYear(){
+		List<Whisky> found = whiskyRepository.findWhiskiesByYear(1991);
+		assertEquals(new Long(2),found.get(0).getId());
+	}
+
+	@Test
+	public void canFindMultipleWhiskiesByYear(){
+		List<Whisky> found = whiskyRepository.findWhiskiesByYear(2018);
+		assertEquals(2, found.size());
 	}
 
 }
